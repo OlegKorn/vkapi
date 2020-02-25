d9c63cbe94eb4f27e1f08222b4682c8a071ff94a
O <48214249+OlegKorn@users.noreply.github.com>
2020-02-25 11:03:59 +0200

    Update photos_get.py

--- photos_get.py
+++ photos_get.py
@@ -3,7 +3,7 @@ import vk_api, re
 import json
 
 
-home = '/home/o/Изображения/VK/fantazy/'
+home = '/home/o/Документы/PYTHON_SCRIPTS/vk/'
 
 vk_session = vk_api.VkApi(
   'phone', 
@@ -18,21 +18,18 @@ headers = {
   'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
 }
 
-access_token = 'dd990119dd990119dd9901198cddf78823ddd99dd9901198068e6f74798845cfd5d1c15'
+access_token = ''
 api_version = '5.103'
 
 vk_session.auth()    
 vk = vk_session.get_api()
 
 album_urls = [
-  'https://vk.com/album-2481783_23140238'
-  'https://vk.com/album-2481783_23140238',
-  'https://vk.com/album-2481783_137160020',
-  'https://vk.com/album-2481783_98052270',
-  'https://vk.com/album-2481783_31701973',
-  'https://vk.com/album-2481783_54358041',
-  'https://vk.com/album-2481783_105879702',
-  'https://vk.com/album-2481783_92237492'
+    'https://vk.com/album-113906974_268901626',
+    'https://vk.com/album-113906974_269029261',
+    'https://vk.com/album-113906974_268790663',
+    'https://vk.com/album-113906974_268366883',
+    'https://vk.com/album-113906974_268366647'
 ]
 
 for album_url in album_urls:
@@ -85,96 +82,3 @@ for album_url in album_urls:
     except Exception as e:
         print(e)
         continue
-
-
-
-
-
-
-
-
-
-
-
-'''
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[74]:
-
-
-import vk_api
-import matplotlib.pyplot as plt
-from collections import Counter
-import numpy as np
-import requests
-
-vk_session = vk_api.VkApi('89246057878', 'QwasQwasQwas')
-vk_session.auth()    
-vk = vk_session.get_api()
-
-city = vk.database.getCities(country_id=1, q="Иркутск")
-city_id = city['items'][0]['id']
-
-
-# In[75]:
-
-
-city_members = vk.users.search(
-    city=city_id, 
-    count=200,
-    fields=[
-        'photo_100', 
-        'bdate',
-        'sex',
-        'relations',
-        'people_main'
-    ]
-)
-
-#print(city_members)
-
-
-# In[76]:
-
-
-city_members = city_members['items']
-all_names = [member['first_name'] for member in city_members]
-#print(all_names)
-
-dict_names = dict(Counter(all_names).most_common(10))
-names = list(range(len(dict_names)))
-values = list(dict_names.values())
-
-
-# In[82]:
-
-
-plt.bar(names, values)
-plt.xticks(names, dict_names.keys(), rotation=90);
-
-
-# In[83]:
-
-
-def save_pic(url):
-    p = requests.get(url)
-    out = open('img.jpg', 'wb')
-    out.write(p.content)
-    out.close()
-    return plt.imread('img.jpg')
-
-
-# In[84]:
-
-
-plt.imshow(save_pic(city_members[0]['photo_100']))
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:'''
